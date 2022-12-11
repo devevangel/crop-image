50fd1efb115a0b6fb426fb14f377b4a4af9c13c9
ChidiBrokeDNet <59733962+devevangel@users.noreply.github.com>
2022-12-11 04:23:20 +0000

    Added starter files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  const [image, setImage] = useState();
+  const [cropData, setCropData] = useState("#");
+  const [cropper, setCropper] = useState();
+  const onChange = (e) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+  };
+
+  const getCropData = () => {
+    if (typeof cropper !== "undefined") {
+      setCropData(cropper.getCroppedCanvas().toDataURL());
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div style={{ width: "100%" }}>
+        <input type="file" onChange={onChange} />
+
+        <br />
+        <br />
+        <Cropper
+          style={{ height: 400, width: "100%" }}
+          zoomTo={0.5}
+          initialAspectRatio={1}
+          preview=".img-preview"
+          src={image}
+          viewMode={1}
+          minCropBoxHeight={10}
+          minCropBoxWidth={10}
+          background={false}
+          responsive={true}
+          autoCropArea={1}
+          checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+          onInitialized={(instance) => {
+            setCropper(instance);
+          }}
+          guides={true}
+        />
+      </div>
+      <div>
+        <div className="box" style={{ width: "50%", float: "right" }}>
+          <h1>Preview</h1>
+          <div
+            className="img-preview"
+            style={{ width: "100%", float: "left", height: "300px" }}
+          />
+        </div>
+        <div
+          className="box"
+          style={{ width: "50%", float: "right", height: "300px" }}
         >
-          Learn React
-        </a>
-      </header>
+          <h1>
+            <span>Crop</span>
+            <button
+              className="btn"
+              style={{ float: "right" }}
+              onClick={getCropData}
+            >
+              Crop Image
+            </button>
+          </h1>
+          <img
+            style={{ width: "100%" }}
+            src={cropData}
+            alt="Cropped item will appear here..."
+          />
+        </div>
+      </div>
+      <br style={{ clear: "both" }} />
     </div>
   );
-}
+};
 
 export default App;
